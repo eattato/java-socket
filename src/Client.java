d8d4858418373158917d6e2e7ada61dd1b489b4e
eattato <80892781+eattato@users.noreply.github.com>
2022-04-13 00:02:46 +0900

    added base code
    Added Server, Client

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.io.*;
+
+public class Client {
+	Socket connection;
+	
+	Client(Socket connection) {
+		this.connection = connection;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
